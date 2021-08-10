4922c46d79397cfb4b23b164e7e1aa34966bbfb8
ByeongIlGod <quddlf410@naver.com>
2021-08-10 15:50:14 +0900

    test1

--- src/basic/ArraysCopyOf.java
+++ src/basic/ArraysCopyOf.java
@@ -6,7 +6,7 @@ public class ArraysCopyOf {
     public static void main(String[] args) {
         String[] days = {"월", "화", "수", "목", "금", "토", "일"};
 
-        String[] days2 = Arrays.copyOf(days, 9);
+        String[] days2 = Arrays.copyOf(days, 8);
 
         days2[7] = "일";
 
