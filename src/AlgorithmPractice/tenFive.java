e1f01621a84a3d0092db0413098aaf968a7ab55c
Rotomoo <quddlf410@naver.com>
2021-08-29 23:38:57 +0900

    test1

---
+++ src/AlgorithmPractice/tenFive.java
@@ -0,0 +1,32 @@
+package AlgorithmPractice;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class tenFive {
+    static int n, m;
+    static int[] dy;
+    public int solution(int[] coin) {
+        Arrays.fill(dy, Integer.MAX_VALUE);
+        dy[0]=0;
+        for (int i=0; i<n; i++) {
+            for (int j = coin[i]; j <= m; j++) {
+                dy[j] = Math.min(dy[j], dy[j - coin[i]] + 1);
+            }
+        }
+        return dy[m];
+    }
+
+    public static void main(String[] args) {
+        tenFive T = new tenFive();
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        int[] coin = new int[n];
+        for (int i=0; i<n; i++) {
+            coin[i]=scanner.nextInt();
+        }
+        m = scanner.nextInt();
+        dy = new int[m+1];
+        System.out.print(T.solution(coin));
+    }
+}
---
+++ src/AlgorithmPractice/tenTwo.java
@@ -0,0 +1,21 @@
+package AlgorithmPractice;
+
+import java.util.Scanner;
+
+class tenTwo {
+    static int[] fibo;
+    public int solution(int n) {
+        fibo[1]=2;
+        fibo[2]=3;
+        for (int i=3;i<=n;i++) fibo[i]=fibo[i-2]+fibo[i-1];
+        return fibo[n];
+    }
+
+    public static void main(String[] args) {
+        tenTwo T = new tenTwo();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        fibo = new int[n+1];
+        System.out.print(T.solution(n));
+    }
+}
