0045e05313fd5266b745cc65d71efb9816cb34d2
Rotomoo <quddlf410@naver.com>
2021-10-27 00:19:03 +0900

    test1

--- src/Backjoon/Dy1932StarStar.java
+++ src/Backjoon/Dy1932StarStar.java
@@ -12,20 +12,21 @@ public class Dy1932StarStar {
         int[][] dy = new int[n][n];
         for (int i=0; i<n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j=0; j<i+1; j++) {
+            for (int j=0; j<=i; j++) {
                 dy[i][j]=Integer.parseInt(st.nextToken());
             }
         }
         for (int i=1; i<n; i++) {
-            for (int j=0; j<i+1; j++) {
-                if (j-1>=0) {
-                    dy[i][j]+=Math.max(dy[i-1][j],dy[i-1][j-1]);
-                }
-                else dy[i][j]+=dy[i-1][j];
+            for (int j=0; j<=i; j++) {
+                if (j==0) dy[i][j]+=dy[i-1][j];
+                else if (j==i) dy[i][j]+=dy[i-1][j-1];
+                else dy[i][j]+=Math.max(dy[i-1][j-1],dy[i-1][j]);
             }
         }
         int answer=Integer.MIN_VALUE;
-        for (int i=0; i<n; i++) answer=Math.max(answer,dy[n-1][i]);
+        for (int i=0; i<n; i++) {
+            answer=Math.max(answer,dy[n-1][i]);
+        }
         System.out.print(answer);
     }
 }
