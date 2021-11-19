8443a207183a4bb158a5854888a1bc97110534c4
Rotomoo <quddlf410@naver.com>
2021-11-19 16:55:17 +0900

    update git

--- src/programmers/Keypad.java
+++ src/programmers/Keypad.java
@@ -1,120 +1,56 @@
 package programmers;
 
 public class Keypad {
-    static class Point {
-        int x,y;
-        Point(int x, int y) {
-            this.x=x;
-            this.y=y;
-        }
-    }
-    public String solution(int[] numbers, String hand) {
+    public String solution(int[] priorities, String hand) {
         String answer = "";
-        Point L = new Point(3,0);
-        Point R = new Point(3,2);
-        for (int n : numbers) {
-            if (n==1 || n==4 || n==7) {
-                answer += "L";
-                if (n==1) L=new Point(0,0);
-                else if (n==4) L=new Point(1,0);
-                else L=new Point(2,0);
+        int[] lH={3,0};
+        int[] rH={3,2};
+        for(int x : priorities) {
+            if (x==1 || x==4 || x==7) {
+                answer+='L';
+                lH=check(x);
             }
-            else if (n==3 || n==6 || n==9) {
-                answer += "R";
-                if (n==3) R=new Point(0,0);
-                else if (n==6) R=new Point(1,0);
-                else R=new Point(2,0);
+            else if (x==3 || x==6 || x==9) {
+                answer+='R';
+                rH=check(x);
             }
             else {
-                if (n==2) {
-                    if (Math.abs(L.x-0)+Math.abs(L.y-1)>Math.abs(R.x-0)+Math.abs(R.y-1)) {
-                        answer+="R";
-                        R=new Point(0,1);
-                    }
-                    else if (Math.abs(L.x-0)+Math.abs(L.y-1)==Math.abs(R.x-0)+Math.abs(R.y-1)) {
-                        if (hand.equals("right")) {
-                            answer+="R";
-                            R=new Point(0,1);
-                        }
-                        else {
-                            answer+="L";
-                            L=new Point(0,1);
-                        }
-                    }
-                    else {
-                        answer+="L";
-                        L=new Point(0,1);
-                    }
-                }
-                else if (n==5) {
-                    if (Math.abs(L.x-1)+Math.abs(L.y-1)>(Math.abs(R.x-1)+Math.abs(R.y-1))) {
-                        answer+="R";
-                        R=new Point(1,1);
-                    }
-                    else if (Math.abs(L.x-1)+Math.abs(L.y-1)==(Math.abs(R.x-1)+Math.abs(R.y-1))) {
-                        if (hand.equals("right")) {
-                            answer+="R";
-                            R=new Point(1,1);
-                        }
-                        else {
-                            answer+="L";
-                            L=new Point(1,1);
-                        }
-                    }
-                    else {
-                        answer+="L";
-                        L=new Point(1,1);
-                    }
-                }
-                else if (n==8) {
-                    if (Math.abs(L.x-2)+Math.abs(L.y-1)>Math.abs(R.x-2)+Math.abs(R.y-1)) {
-                        answer+="R";
-                        R=new Point(2,1);
-                    }
-                    else if (Math.abs(L.x-2)+Math.abs(L.y-1)==Math.abs(R.x-2)+Math.abs(R.y-1)) {
-                        if (hand.equals("right")) {
-                            answer+="R";
-                            R=new Point(2,1);
-                        }
-                        else {
-                            answer+="L";
-                            L=new Point(2,1);
-                        }
-                    }
-                    else {
-                        answer+="L";
-                        L=new Point(2,1);
-                    }
+                int[] tmpxy=check(x);
+                if (pri(tmpxy,lH,rH,hand)=='R') {
+                    answer+='R';
+                    rH=tmpxy;
                 }
                 else {
-                    if (Math.abs(L.x-3)+Math.abs(L.y-1)>Math.abs(R.x-3)+Math.abs(R.y-1)) {
-                        answer+="R";
-                        R=new Point(3,1);
-                    }
-                    else if (Math.abs(L.x-3)+Math.abs(L.y-1)==Math.abs(R.x-3)+Math.abs(R.y-1)) {
-                        if (hand.equals("right")) {
-                            answer+="R";
-                            R=new Point(3,1);
-                        }
-                        else {
-                            answer+="L";
-                            L=new Point(3,1);
-                        }
-                    }
-                    else {
-                        answer+="L";
-                        L=new Point(3,1);
-                    }
+                    answer+='L';
+                    lH=tmpxy;
                 }
             }
         }
         return answer;
     }
 
+    public int[] check(int x) {
+        int[][] numxy={{3,1},{0,0},{0,1},{0,2},{1,0},{1,1},{1,2},{2,0},{2,1},{2,2}};
+        return numxy[x];
+    }
+
+    public char pri(int[] tmpxy, int[] lH, int[] rH,String hand) {
+        char tmp=' ';
+        int ldis=Math.abs(lH[0]-tmpxy[0])+Math.abs(lH[1]-tmpxy[1]);
+        int rdis=Math.abs(rH[0]-tmpxy[0])+Math.abs(rH[1]-tmpxy[1]);
+        if (ldis>rdis) tmp='R';
+        else if (ldis<rdis) tmp='L';
+        else {
+            if (hand.equals("right")) tmp='R';
+            else tmp='L';
+        }
+        return tmp;
+    }
+
     public static void main(String[] args) {
         Keypad T = new Keypad();
-        int[] numbers={1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
-        String hand="right";
-        System.out.println(T.solution(numbers,hand));
+        int[] priorities={7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2};
+        String hand="left";
+        System.out.println(T.solution(priorities,hand));
     }
 }
