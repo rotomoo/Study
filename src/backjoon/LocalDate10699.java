c846cebb46ea3414a4a28d35a945ad3b577aafd3
“rotomoo” <quddlf410@naver.com>
2021-12-22 18:48:47 +0900

    test: add LocalDate problem

---
+++ src/backjoon/LocalDate10699.java
@@ -0,0 +1,14 @@
+package backjoon;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class LocalDate10699 {
+    public static void main(String[] args) throws IOException {
+        LocalDate a = LocalDate.now();
+        System.out.println(a);
+//        System.out.println(a.getYear());
+//        System.out.println(a.getMonthValue());
+//        System.out.println(a.getDayOfMonth());
+    }
+}
