bd4214f7cf658b23d8a2bcccb9dcbb5a6009425a
“rotomoo” <quddlf410@naver.com>
2021-12-22 18:17:36 +0900

    test: add Math problem

---
+++ src/backjoon/Math15894.java
@@ -0,0 +1,12 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Math15894 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Long.parseLong(br.readLine()) * 4);
+    }
+}
