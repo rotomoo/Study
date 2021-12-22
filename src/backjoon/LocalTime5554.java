f96ff09c13d94bd76275b4d8583fd7f58b0558e0
“rotomoo” <quddlf410@naver.com>
2021-12-22 18:48:56 +0900

    test: add LocalTime problem

--- src/backjoon/LocalTime5554.java
+++ src/backjoon/LocalTime5554.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 import java.time.Duration;
 import java.time.LocalTime;
 
-public class Time5554 {
+public class LocalTime5554 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int tmp = 0;
