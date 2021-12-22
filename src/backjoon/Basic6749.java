a6226eeb06aee99883f4f4a6105ed5c88952290d
“rotomoo” <quddlf410@naver.com>
2021-12-22 19:53:22 +0900

    test: add Basic problem

---
+++ src/backjoon/Basic6749.java
@@ -0,0 +1,14 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Basic6749 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        System.out.println(2*b-a);
+    }
+}
