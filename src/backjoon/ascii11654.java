6f2518d7ab88f4b381c2202244cae7996162fb92
“rotomoo” <quddlf410@naver.com>
2021-12-22 13:51:32 +0900

    test: add ascii problem

---
+++ src/backjoon/ascii11654.java
@@ -0,0 +1,12 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ascii11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println((int)br.readLine().charAt(0));
+    }
+}
