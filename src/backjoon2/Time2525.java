bfc05c5411dbce496923fadd783a95b8f5356e6f
“rotomoo” <quddlf410@naver.com>
2021-12-23 16:12:27 +0900

    test: add Time

---
+++ src/backjoon2/Time2525.java
@@ -0,0 +1,20 @@
+package backjoon2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Time2525 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int hour = Integer.parseInt(st.nextToken());
+        int min = Integer.parseInt(st.nextToken());
+        min += Integer.parseInt(br.readLine());
+        System.out.println((hour+min/60)%24+" "+min%60);
+//        int tmp = hour*60 + min;
+//        if (tmp >= 1440) tmp -= 1440;
+//        System.out.println(tmp/60 +" " + tmp%60);
+    }
+}
