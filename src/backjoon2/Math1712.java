1aa610979651b51b58e9fc5810b885bfd9f02eef
“rotomoo” <quddlf410@naver.com>
2021-12-23 15:06:47 +0900

    test: add math

---
+++ src/backjoon2/Math1712.java
@@ -0,0 +1,21 @@
+package backjoon2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Math1712 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        if (b>=c){
+            System.out.println(-1);
+            System.exit(0);
+        }
+        System.out.println(a/(c-b)+1);
+    }
+}
