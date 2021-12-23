dd36ce0440a3decdf5e6143904d82eef4e550ca1
“rotomoo” <quddlf410@naver.com>
2021-12-23 23:41:39 +0900

    test: add time

--- src/backjoon2/Time2525.java
+++ src/backjoon2/Time2525.java
@@ -10,9 +10,9 @@ public class Time2525 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int hour = Integer.parseInt(st.nextToken());
-        int min = Integer.parseInt(st.nextToken());
-        min += Integer.parseInt(br.readLine());
-        System.out.println((hour+min/60)%24+" "+min%60);
+        int min = Integer.parseInt(st.nextToken()) + Integer.parseInt(br.readLine());
+        hour += min/60;
+        System.out.println(hour%24+" "+min%60);
 //        int tmp = hour*60 + min;
 //        if (tmp >= 1440) tmp -= 1440;
 //        System.out.println(tmp/60 +" " + tmp%60);
