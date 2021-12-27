4e1746c485d23ccf0e8a29a70f7334c2273342c6
“rotomoo” <quddlf410@naver.com>
2021-12-27 20:55:33 +0900

    test: add basic

--- src/backjoon2/Answer.java
+++ src/backjoon2/Answer.java
@@ -3,6 +3,7 @@ package backjoon2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Answer {
     public static void main(String[] args) throws IOException {
