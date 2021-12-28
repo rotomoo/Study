a74e2b3f735c8644905e0299a779611f87253ec4
“rotomoo” <quddlf410@naver.com>
2021-12-28 17:36:31 +0900

    test: add Sort

---
+++ src/backjoon2/Sort2751.java
@@ -0,0 +1,20 @@
+package backjoon2;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Sort2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++) {
+            arr[i]=Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        for (int x : arr) bw.write(x+"\n");
+        bw.flush();
+        bw.close();
+    }
+}
