38c50aaad82deaaba28ef6707902d93280a1a4f0
“rotomoo” <quddlf410@naver.com>
2021-12-30 18:24:37 +0900

    test: add String

--- src/programmers/String3.java
+++ src/programmers/String3.java
@@ -4,7 +4,7 @@ public class String3 {
     public int solution(java.lang.String s) {
         java.lang.String[] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         for (int i=0; i<10; i++) {
-            s = s.replace(arr[i], java.lang.String.valueOf(i));
+            s = s.replace(arr[i], String.valueOf(i));
         }
         return Integer.parseInt(s);
     }
