a71a094c98aff3dc6abf92e240fa803c2a4f0925
“rotomoo” <quddlf410@naver.com>
2021-12-30 18:24:27 +0900

    test: add decimal Conversion

---
+++ src/programmers/Decimal124.java
@@ -0,0 +1,19 @@
+package programmers;
+
+class Decimal124 {
+    public static String solution(int n) {
+        String[] num = {"4", "1", "2"};
+        String answer = "";
+
+        while (n > 0) {
+            answer = num[n % 3] + answer;
+            n = (n - 1) / 3;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(6));
+    }
+}
