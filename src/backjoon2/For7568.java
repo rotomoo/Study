91cc04ddf285039c772861be91fe9df49942c41f
“rotomoo” <quddlf410@naver.com>
2022-01-04 11:42:44 +0900

    test: add for

--- src/backjoon2/For7568.java
+++ src/backjoon2/For7568.java
@@ -7,32 +7,58 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class For7568 {
-    static class Point{
-        int we, he;
-        Point(int we, int he) {
-            this.we = we;
-            this.he = he;
+    static class Point {
+        int x, y;
+        Point(int x, int y) {
+            this.x=x;
+            this.y=y;
         }
     }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        ArrayList<Point> list = new ArrayList<>();
+        Point[] arr = new Point[n];
         for (int i=0; i<n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            list.add(new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+            arr[i] = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         }
-        int[] answer = new int[n];
         for (int i=0; i<n; i++) {
-            int score = 1;
+            int cnt = 1;
             for (int j=0; j<n; j++) {
-                if (list.get(i).he < list.get(j).he && list.get(i).we < list.get(j).we) {
-                    score++;
+                if (arr[i].x < arr[j].x && arr[i].y < arr[j].y) {
+                    cnt++;
                 }
             }
-            answer[i] = score;
+            System.out.print(cnt+" ");
         }
-        for (int x : answer) System.out.print(x+" ");
     }
+
+//    static class Point{
+//        int we, he;
+//        Point(int we, int he) {
+//            this.we = we;
+//            this.he = he;
+//        }
+//    }
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int n = Integer.parseInt(br.readLine());
+//        ArrayList<Point> list = new ArrayList<>();
+//        for (int i=0; i<n; i++) {
+//            StringTokenizer st = new StringTokenizer(br.readLine());
+//            list.add(new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+//        }
+//        int[] answer = new int[n];
+//        for (int i=0; i<n; i++) {
+//            int score = 1;
+//            for (int j=0; j<n; j++) {
+//                if (list.get(i).he < list.get(j).he && list.get(i).we < list.get(j).we) {
+//                    score++;
+//                }
+//            }
+//            answer[i] = score;
+//        }
+//        for (int x : answer) System.out.print(x+" ");
+//    }
 }
