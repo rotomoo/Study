607019a18b74c421b6419633929cc27c2182fcdf
“rotomoo” <quddlf410@naver.com>
2022-01-23 13:40:41 +0900

    test: add problem

--- src/programmers/DartGame.java
+++ src/programmers/DartGame.java
@@ -1,43 +1,34 @@
 package programmers;
 
-import java.util.*;
+import java.util.Stack;
 
 class DartGame {
     public int solution(String dartResult) {
+        int answer=0;
         Stack<Integer> stack = new Stack<>();
-        int sum = 0;
-        for (int i = 0; i < dartResult.length(); ++i) {
-            char c = dartResult.charAt(i);
-            if (Character.isDigit(c)) {
-                sum = (c - '0');
-                if (sum == 1 && i < dartResult.length() - 1 && dartResult.charAt(i + 1) == '0') {
-                    sum = 10;
+        for (int i=0; i<dartResult.length(); i++) {
+            if (Character.isDigit(dartResult.charAt(i))) {
+                if (dartResult.charAt(i) == '1' && dartResult.charAt(i+1) == '0') {
+                    stack.push(10);
                     i++;
                 }
-                stack.push(sum);
-            } else {
-                int prev = stack.pop();
-                if (c == 'D') {
-                    prev *= prev;
-                } else if (c == 'T') {
-                    prev = prev * prev * prev;
-                } else if (c == '*') {
-                    if (!stack.isEmpty()) {
-                        int val = stack.pop() * 2;
-                        stack.push(val);
-                    }
-                    prev *= 2;
-                } else if (c == '#') {
-                    prev *= (-1);
+                else {
+                    stack.push(dartResult.charAt(i) - '0');
                 }
-                // System.out.println(prev);
-                stack.push(prev);
+            }
+            else {
+                int tmp = stack.pop();
+                if (dartResult.charAt(i) == 'S') stack.push(tmp);
+                else if (dartResult.charAt(i) == 'D') stack.push(tmp*tmp);
+                else if (dartResult.charAt(i) == 'T') stack.push(tmp*tmp*tmp);
+                else if (dartResult.charAt(i) == '#') stack.push(-tmp);
+                else if (dartResult.charAt(i) == '*') {
+                    if (!stack.isEmpty()) stack.push(stack.pop() * 2);
+                    stack.push(tmp * 2);
+                }
             }
         }
-        int totalScore = 0;
-        while (!stack.isEmpty()) {
-            totalScore += stack.pop();
-        }
-        return totalScore;
+        while (!stack.isEmpty()) answer += stack.pop();
+        return answer;
     }
 }
