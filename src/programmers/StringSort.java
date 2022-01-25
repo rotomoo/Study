249e8de63d4c5e7e7071f073e81fae3b4cb951df
“rotomoo” <quddlf410@naver.com>
2022-01-25 14:42:17 +0900

    test: add problem

---
+++ src/programmers/StringSort.java
@@ -0,0 +1,21 @@
+package programmers;
+
+import java.util.Arrays;
+
+class StringSort {
+    public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, (o1, o2) ->
+            o1.charAt(n) == o2.charAt(n) ? o1.compareTo(o2) : o1.charAt(n) - o2.charAt(n));
+        return strings;
+    }
+}
+
+//class Solution {
+//    public String[] solution(String[] strings, int n) {
+//        Arrays.sort(strings, ((o1, o2) -> {
+//            if (o1.charAt(n) == o2.charAt(n)) return o1.compareTo(o2);
+//            else return o1.charAt(n) - o2.charAt(n);
+//        }));
+//        return strings;
+//    }
+//}
