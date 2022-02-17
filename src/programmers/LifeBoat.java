3fee92e40f1b0c094365001339f3206fd2019619
“rotomoo” <quddlf410@naver.com>
2022-02-17 18:25:55 +0900

    test: add problem

--- src/programmers/LifeBoat.java
+++ src/programmers/LifeBoat.java
@@ -2,21 +2,23 @@ package programmers;
 
 import java.util.Arrays;
 
-public class Greedy4 {
+class LifeBoat {
     public static int solution(int[] people, int limit) {
         int answer = 0;
         Arrays.sort(people);
         int lt = 0;
         for (int rt=people.length-1; lt<=rt; rt--){
+            if (lt==rt) {
+                answer++;
+                break;
+            }
             if (people[lt]+people[rt]<=limit) lt++;
             answer++;
         }
         return answer;
     }
 
-    public static void main(String[] args) {;
-        int[] people={30,40,60,70};
-        int limit=100;
-        System.out.println(solution(people,limit));
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{70, 50, 80, 50}, 100));
     }
 }
