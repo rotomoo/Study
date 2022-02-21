bf488ac3161e67a6caeeb84bb39340617299b24b
“rotomoo” <quddlf410@naver.com>
2022-02-21 14:12:02 +0900

    test: add problem

--- src/programmers/BigSqure.java
+++ src/programmers/BigSqure.java
@@ -1,23 +1,18 @@
 package programmers;
 
-class BigSqure
-{
-    public int solution(int [][]board)
-    {
+class BigSqure {
+    public int solution(int [][]board) {
         int answer = Integer.MIN_VALUE;
-        if (board.length == 1 || board[0].length == 1) {
-            for (int i = 0; i < board.length; i++) {
-                for (int j = 0; j < board[0].length; j++) {
-                    answer = Math.max(answer, board[i][j]);
-                }
-            }
-        }
+        int[][] dy = board;
         for (int i = 1; i < board.length; i++) {
             for (int j = 1; j < board[0].length; j++) {
-                if (board[i][j]==1) {
-                    board[i][j] = Math.min(board[i - 1][j - 1], Math.min(board[i - 1][j], board[i][j - 1])) + 1;
-                    answer = Math.max(answer, board[i][j]);
-                }
+                if (board[i][j]==1) dy[i][j] = Math.min(dy[i - 1][j], Math.min(dy[i][j - 1], dy[i - 1][j - 1])) + 1;
+                answer = Math.max(answer, dy[i][j]);
+            }
+        }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                answer = Math.max(answer, dy[i][j]);
             }
         }
         return answer * answer;
