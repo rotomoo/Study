d72a114c0560a215cdb45c0b0ca42939e3de8917
“rotomoo” <quddlf410@naver.com>
2022-02-21 15:17:18 +0900

    test: add problem

--- src/programmers/Immigration.java
+++ src/programmers/Immigration.java
@@ -4,27 +4,19 @@ import java.util.Arrays;
 
 public class Immigration {
     public long solution(int n, int[] times) {
-        long answer = 0;
+        long answer = Long.MAX_VALUE;
         Arrays.sort(times);
-        long lt=times[0];
-        long rt=n*times[times.length-1];
-        while (lt<=rt) {
-            long mid=(lt+rt)/2;
-            long cnt=0;
-            for (int i=0; i<times.length; i++) cnt+=mid/times[i];
-            if (cnt>=n) {
-                answer=mid;
-                rt=mid-1;
-            }
-            else lt=mid+1;
+        long lt = times[0];
+        long rt = n * times[times.length - 1];
+        while (lt <= rt) {
+            long mid = (lt + rt) / 2;
+            long cnt = 0;
+            for (int i = 0; i < times.length; i++) cnt += mid / times[i];
+            if (cnt >= n) {
+                answer = mid;
+                rt = mid - 1;
+            } else lt = mid + 1;
         }
         return answer;
-    }
-
-    public static void main(String[] args) {
-        Immigration T = new Immigration();
-        int[] priorities={2,1,3,2};
-        int location=2;
-        System.out.println(T.solution(location,priorities));
     }
 }
