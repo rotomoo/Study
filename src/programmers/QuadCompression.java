b27eaceb50067d0be51c4f34eed91fd9c86a5ffd
“rotomoo” <quddlf410@naver.com>
2022-02-22 13:11:01 +0900

    test: add problem

--- src/programmers/QuadCompression.java
+++ src/programmers/QuadCompression.java
@@ -1,37 +1,33 @@
 package programmers;
 
 class QuadCompression {
-    int[] answer;
+    static int[] answer;
+    static int[][] arrTmp;
 
-    public void DFS(int len, int x, int y, int[][] arr) {
-        if(len == 1) {
-            answer[arr[x][y]]++;
+    public static void DFS(int len, int x, int y) {
+        if (isBlock(len, x, y) || len == 1) {
+            answer[arrTmp[x][y]]++;
             return;
         }
-
-        if(isBlock(len, x, y, arr)) return;
-
-        DFS(len/2, x, y, arr);
-        DFS(len/2, x, y + len/2, arr);
-        DFS(len/2, x + len/2, y, arr);
-        DFS(len/2, x + len/2, y + len/2, arr);
-
+        DFS(len / 2, x, y);
+        DFS(len / 2, x + len / 2, y);
+        DFS(len / 2, x, y + len / 2);
+        DFS(len / 2, x + len / 2, y + len / 2);
     }
 
-    public boolean isBlock(int len, int x, int y, int[][] arr) {
-        for (int i = x; i < x + len; i++) {
-            for (int j = y; j < y + len; j++) {
-                if(arr[x][y] != arr[i][j]) return false;
+    public static boolean isBlock(int len, int x, int y) {
+        for (int i = x; i < len + x; i++) {
+            for (int j = y; j < len + y; j++) {
+                if (arrTmp[x][y] != arrTmp[i][j]) return false;
             }
         }
-
-        answer[arr[x][y]]++;
         return true;
     }
 
     public int[] solution(int[][] arr) {
         answer = new int[2];
-        DFS(arr.length, 0, 0, arr);
+        arrTmp = arr;
+        DFS(arr.length, 0, 0);
         return answer;
     }
 }
