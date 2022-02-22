2eadf268bbc61d05dd1a870bab44daedf3645d23
“rotomoo” <quddlf410@naver.com>
2022-02-22 13:43:07 +0900

    test: add problem

--- src/programmers/SliceArray.java
+++ src/programmers/SliceArray.java
@@ -14,19 +14,52 @@ class SliceArray {
 
 //class Solution {
 //    public int[] solution(int n, long left, long right) {
+//        int[] tmp = new int[(int)(right-left+1)];
+//        for (int i = (int)left; i < right+1; i++) {
+//            tmp[(int)(i-left)] = Math.max(i/n, i%n)+1;
+//        }
+//        return tmp;
+//    }
+//}
+
+//class Solution {
+//    public int[] solution(int n, long left, long right) {
 //        int[] answer = {};
-//        int[][] graph = new int[n+1][n+1];
+//        int[][] graph = new int[n][n];
 //        for (int i = 0; i < n; i++) {
 //            for (int j = 0; j < n; j++) {
 //                graph[i][j] = Math.max(i, j)+1;
 //            }
 //        }
-//        int[] tmp = new int[(int)(right - left + 1)];
+//        int[] tmp = new int[(int)(right-left+1)];
 //        for (int i = (int)left; i < right+1; i++) {
-//            int row = i / n;
-//            int col = i % n;
-//            tmp[i - (int) left] = Math.max(i / n, i % n);
+//            tmp[(int)(i-left)] = graph[i / n][i % n];
 //        }
 //        return tmp;
+//    }
+//}
+
+//class Solution {
+//    public int[] solution(int n, long left, long right) {
+//        int[] answer = {};
+//        int[] tmp = new int[n * n];
+//        int[][] graph = new int[n][n];
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n; j++) {
+//                graph[i][j] = Math.max(i, j)+1;
+//                System.out.print(graph[i][j]+" ");
+//            }
+//            System.out.println();
+//        }
+//
+//        int row = 0;
+//        int col = 0;
+//        for (int i = 0; i < n * n; i++) {
+//            row = i / n;
+//            col = i % n;
+//            tmp[i] = graph[row][col];
+//            System.out.print(tmp[i]);
+//        }
+//        return Arrays.copyOfRange(tmp, (int)left, (int)right+1);
 //    }
 //}
