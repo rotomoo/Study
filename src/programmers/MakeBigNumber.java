18326ee8268d22b35fa53a68bc2490c008dc5695
“rotomoo” <quddlf410@naver.com>
2022-02-25 13:41:22 +0900

    test: add problem

--- src/programmers/MakeBigNumber.java
+++ src/programmers/MakeBigNumber.java
@@ -1,5 +1,25 @@
 package programmers;
 
+import java.util.Stack;
+
+class MakeBigNumber {
+    public String solution(String number, int k) {
+        StringBuilder answer = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+        for (char x : number.toCharArray()) {
+            while (!stack.isEmpty() && stack.peek() < x && k > 0) {
+                stack.pop();
+                k--;
+            }
+            stack.push(x);
+        }
+        for (; k > 0; k--) stack.pop();
+        for (char x : stack) answer.append(x);
+        return answer.toString();
+    }
+}
+
+/*
 import java.io.IOException;
 
 public class Greedy3 {
@@ -33,3 +53,4 @@ public class Greedy3 {
         System.out.println(solution(number,k));
     }
 }
+*/
