de975e2d0340a915c2fe535d84e8281235518a28
“rotomoo” <quddlf410@naver.com>
2022-03-03 15:56:01 +0900

    test: add problem

--- src/backjoon/DfsBfs1260.java
+++ src/backjoon/DfsBfs1260.java
@@ -3,10 +3,7 @@ package backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class DfsBfs1260 {
     static int[] ch;
@@ -60,4 +57,57 @@ public class DfsBfs1260 {
         ch[v]=1;
         BFS(v);
     }
+
+/*    static int n, m;
+    static int[] ch;
+    static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+
+    public static void BFS(int v) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(v);
+        while (!q.isEmpty()) {
+            int tmp = q.poll();
+            if (ch[tmp]==1) continue;
+            ch[tmp]=1;
+            System.out.print(tmp+" ");
+            for(int x : list.get(tmp)) {
+                if(ch[x]==0) {
+                    q.offer(x);
+                }
+            }
+        }
+    }
+
+    public static void DFS(int v) {
+        System.out.print(v+" ");
+        ch[v] = 1;
+        for (int x: list.get(v)) {
+            if (ch[x]==0) {
+                DFS(x);
+            }
+        }
+    }
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+        for (int i=0; i<=n; i++) {
+            list.add(new ArrayList<Integer>());
+        }
+        for (int i=0; i<m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list.get(a).add(b);
+            list.get(b).add(a);
+        }
+        ch = new int[n+1];
+        DFS(v);
+        System.out.println();
+        ch = new int[n+1];
+        BFS(v);
+    }*/
 }
