e68b73319753c113798f484a96f980d318a76b6b
“rotomoo” <quddlf410@naver.com>
2022-03-08 18:26:00 +0900

    test: add problem

--- src/programmers/HIndex.java
+++ src/programmers/HIndex.java
@@ -3,15 +3,32 @@ package programmers;
 import java.util.Arrays;
 
 class HIndex {
-    public int solution(int[] citations) {
+    public static int solution(int[] citations) {
+        int answer = 0;
+        int h = 0;
         Arrays.sort(citations);
-        int max = 0;
-        for (int i = citations.length-1; i >= 0; i--) {
-            int min = Math.min(citations[i], citations.length - i);
-            max = Math.max(max, min);
+        while (true) {
+            int cnt = 0;
+            for (int i = citations.length-1; i >= 0 ; i--) {
+                if (h > citations[i]) break;
+                cnt++;
+            }
+            if (cnt >= h) answer = Math.max(answer, h);
+            else break;
+            h++;
         }
-        return max;
+        return answer;
     }
+
+//    public int solution(int[] citations) {
+//        Arrays.sort(citations);
+//        int max = 0;
+//        for (int i = citations.length-1; i >= 0; i--) {
+//            int min = Math.min(citations[i], citations.length - i);
+//            max = Math.max(max, min);
+//        }
+//        return max;
+//    }
 
 //    public int solution(int[] arr) {
 //        int answer=0;
