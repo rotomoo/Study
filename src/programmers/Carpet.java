9b74eaa7d8ca40a74a236d0eff0f016af28d1312
“rotomoo” <quddlf410@naver.com>
2022-03-08 17:57:13 +0900

    test: add problem

--- src/programmers/Carpet.java
+++ src/programmers/Carpet.java
@@ -3,17 +3,16 @@ package programmers;
 class Carpet {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        int sum = brown + yellow;
-        for (int i = 3; i < sum; i++) {
-            if (sum % i == 0) {
-                int width = i;
-                int length = sum / i;
-                if (length > width) continue;
-                if ((width-2) * (length-2) == yellow) {
-                    answer[0] = width;
-                    answer[1] = length;
-                    break;
-                }
+        int s = brown + yellow;
+        for (int i = 3; i < s; i++) {
+            if (s%i!=0) continue;
+            int x = i;
+            int y = s / i;
+            if (x<y) continue;
+            if ((x - 2) * (y - 2) == yellow) {
+                answer[0]=x;
+                answer[1]=y;
+                break;
             }
         }
         return answer;
