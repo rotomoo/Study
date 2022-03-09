f5d1bb08398d2df4cf354a470d282a60eb5da966
“rotomoo” <quddlf410@naver.com>
2022-03-09 14:24:17 +0900

    test: add problem

--- src/programmers/Camouflage.java
+++ src/programmers/Camouflage.java
@@ -6,19 +6,31 @@ import java.util.HashSet;
 public class Camouflage {
 
     public int solution(String[][] clothes) {
-        HashMap<String, HashSet<String>> map = new HashMap<>();
+        int answer = 1;
+        HashMap<String, Integer> map = new HashMap<>();
         for (String[] x : clothes) {
-            HashSet<String> tmp = map.getOrDefault(x[1], new HashSet<>());
-            tmp.add(x[0]);
-            map.put(x[1], tmp);
+            map.put(x[1], map.getOrDefault(x[1], 0) + 1);
         }
-        System.out.println(map);
-        int tmp = 1;
-        for (HashSet<String> val : map.values()) {
-            tmp *= val.size() + 1;
+        for (int x : map.values()) {
+            answer *= (x+1);
         }
-        return tmp-1;
+        return answer - 1;
     }
+
+//    public int solution(String[][] clothes) {
+//        HashMap<String, HashSet<String>> map = new HashMap<>();
+//        for (String[] x : clothes) {
+//            HashSet<String> tmp = map.getOrDefault(x[1], new HashSet<>());
+//            tmp.add(x[0]);
+//            map.put(x[1], tmp);
+//        }
+//        System.out.println(map);
+//        int tmp = 1;
+//        for (HashSet<String> val : map.values()) {
+//            tmp *= val.size() + 1;
+//        }
+//        return tmp-1;
+//    }
 
 //    public static int solution(String[][] clothes) {
 //        int answer = 1;
