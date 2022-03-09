658e3dacb441f0a52fde5195134c1eb7d11a5e62
“rotomoo” <quddlf410@naver.com>
2022-03-09 15:34:13 +0900

    test: add problem

--- src/programmers/Delivery.java
+++ src/programmers/Delivery.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 
 class Delivery {
-    static class Point implements Comparable<Point>{
+    static class Point implements Comparable<Solution.Point> {
         int vex, cost;
 
         public Point(int vex, int cost) {
@@ -14,48 +14,93 @@ class Delivery {
         }
 
         @Override
-        public int compareTo(Point o) {
+        public int compareTo(Solution.Point o) {
             return this.cost - o.cost;
-        }
-    }
-    static int[] result;
-    static ArrayList<ArrayList<Point>> graph = new ArrayList<>();
-
-    public static void BFS(int v) {
-        PriorityQueue<Point> pq = new PriorityQueue<>();
-        pq.offer(new Point(v, 0));
-        while (!pq.isEmpty()) {
-            Point tmp = pq.poll();
-            if (tmp.cost > result[tmp.vex]) continue;
-            for (Point x : graph.get(tmp.vex)) {
-                if (result[x.vex] > tmp.cost + x.cost) {
-                    result[x.vex] = tmp.cost + x.cost;
-                    pq.offer(new Point(x.vex, tmp.cost + x.cost));
-                }
-            }
-
         }
     }
 
     public int solution(int N, int[][] road, int K) {
-        result = new int[N + 1];
+        int answer = 0;
+        int[] result = new int[N + 1];
+        ArrayList<ArrayList<Solution.Point>> list = new ArrayList<>();
         for (int i = 0; i <= N; i++) {
-            graph.add(new ArrayList<>());
+            list.add(new ArrayList<>());
         }
         Arrays.fill(result, Integer.MAX_VALUE);
-        result[1]=0;
-        for (int i = 0; i < road.length; i++) {
-            int a = road[i][0];
-            int b = road[i][1];
-            int c = road[i][2];
-            graph.get(a).add(new Point(b, c));
-            graph.get(b).add(new Point(a, c));
+        result[1] = 0;
+        for (int[] x : road) {
+            list.get(x[0]).add(new Solution.Point(x[1], x[2]));
+            list.get(x[1]).add(new Solution.Point(x[0], x[2]));
         }
-        BFS(1);
-        int answer = 1;
-        for (int i = 2; i <= N; i++) {
-            if (result[i] <= K) answer++;
+        PriorityQueue<Solution.Point> pq = new PriorityQueue<>();
+        pq.offer(new Solution.Point(1, 0));
+        while (!pq.isEmpty()) {
+            Solution.Point tmp = pq.poll();
+            int nowVex = tmp.vex;
+            int nowCost = tmp.cost;
+            for (Solution.Point ob : list.get(nowVex)) {
+                if (result[ob.vex] > nowCost + ob.cost) {
+                    result[ob.vex] = nowCost + ob.cost;
+                    pq.offer(new Solution.Point(ob.vex, nowCost + ob.cost));
+                }
+            }
         }
+
+        for (int x: result) if (K >= x) answer++;
         return answer;
     }
+
+//    static class Point implements Comparable<Point>{
+//        int vex, cost;
+//
+//        public Point(int vex, int cost) {
+//            this.vex = vex;
+//            this.cost = cost;
+//        }
+//
+//        @Override
+//        public int compareTo(Point o) {
+//            return this.cost - o.cost;
+//        }
+//    }
+//    static int[] result;
+//    static ArrayList<ArrayList<Point>> graph = new ArrayList<>();
+//
+//    public static void BFS(int v) {
+//        PriorityQueue<Point> pq = new PriorityQueue<>();
+//        pq.offer(new Point(v, 0));
+//        while (!pq.isEmpty()) {
+//            Point tmp = pq.poll();
+//            if (tmp.cost > result[tmp.vex]) continue;
+//            for (Point x : graph.get(tmp.vex)) {
+//                if (result[x.vex] > tmp.cost + x.cost) {
+//                    result[x.vex] = tmp.cost + x.cost;
+//                    pq.offer(new Point(x.vex, tmp.cost + x.cost));
+//                }
+//            }
+//
+//        }
+//    }
+//
+//    public int solution(int N, int[][] road, int K) {
+//        result = new int[N + 1];
+//        for (int i = 0; i <= N; i++) {
+//            graph.add(new ArrayList<>());
+//        }
+//        Arrays.fill(result, Integer.MAX_VALUE);
+//        result[1]=0;
+//        for (int i = 0; i < road.length; i++) {
+//            int a = road[i][0];
+//            int b = road[i][1];
+//            int c = road[i][2];
+//            graph.get(a).add(new Point(b, c));
+//            graph.get(b).add(new Point(a, c));
+//        }
+//        BFS(1);
+//        int answer = 1;
+//        for (int i = 2; i <= N; i++) {
+//            if (result[i] <= K) answer++;
+//        }
+//        return answer;
+//    }
 }
