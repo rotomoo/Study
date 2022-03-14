c0e93014cfe1546d319548631195f75889ee58c7
“rotomoo” <quddlf410@naver.com>
2022-03-14 13:45:16 +0900

    test: add problem

--- src/programmers2/KMSolution3.java
+++ src/programmers2/KMSolution3.java
@@ -5,20 +5,23 @@ import java.util.Arrays;
 class KMSolution3 {
     public static int solution(int[] A) {
         int[] dy = new int[A.length];
-        if (A.length ==0 || A.length==1) return 1;
+        if (A.length<=2) return 1;
         Arrays.fill(dy, 1);
         int answer = Integer.MIN_VALUE;
         for (int i = 2; i < A.length; i++) {
             if (A[i-2] == A[i]) {
-                dy[i] = Math.max(dy[i - 1] + 1, dy[i - 2] + 2);
+                if (dy[i-2] > 1 && dy[i-1]==1) dy[i] = dy[i-1] + 2;
+                else dy[i] = Math.max(dy[i - 1] + 1, dy[i - 2] + 2);
                 answer = Math.max(answer, dy[i]);
             }
+            System.out.print(dy[i]+" ");
         }
+        System.out.println();
         return answer;
     }
 
 
     public static void main(String[] args) {
-        System.out.println(solution(new int[]{-1, 0, 5, 0, 3, 1, 3, 1}));
+        System.out.println(solution(new int[]{-1,0,-1,0,-1,0,2,0,2,2,2}));
     }
 }
