3ed8ef527733b9c19f099371a32d08418be6ff90
“rotomoo” <quddlf410@naver.com>
2022-07-20 13:36:43 +0900

    test: add problem

--- src/etc/DfsBfs.java
+++ src/etc/DfsBfs.java
@@ -7,7 +7,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class DfsBfs {
     static int n, m;
     static int[][] map;
     static int[] ch;
