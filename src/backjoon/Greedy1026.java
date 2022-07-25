4185a943825438e127023f301ce5d3f863e2e267
“rotomoo” <quddlf410@naver.com>
2022-07-25 15:20:59 +0900

    refactor : Remove comments

--- src/backjoon/Greedy1026.java
+++ src/backjoon/Greedy1026.java
@@ -8,57 +8,38 @@ import java.util.*;
 public class Greedy1026 {
     public static void main(String[] args) throws IOException {
 
-        // buffer 선언
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        /**
-         * 입력 받기
-         */
         int n = Integer.parseInt(br.readLine());
 
-        // a, b 배열 선언
         int[] a = new int[n];
         int[] b = new int[n];
 
-        // 다음줄 받기
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // 배열 입력
         for (int i = 0; i < n; i++) {
             a[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 다음줄 받기
         st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < n; i++) {
             b[i] = Integer.parseInt(st.nextToken());
         }
 
-        /**
-         * 구현
-         */
-
-        // b 배열 리스트화 (stream api 사용할경우 List<Integer> bList = Arrays.stream(b).boxed().collect(Collectors.toList());)
+        // == List<Integer> bList = Arrays.stream(b).boxed().collect(Collectors.toList());
         ArrayList<Integer> bList = new ArrayList<>();
         for (int i = 0; i < b.length; i++) {
             bList.add(b[i]);
         }
 
-        // a배열 정렬
         Arrays.sort(a);
 
         int answer = 0;
 
-        // a의 최소값부터
         for (int i = 0; i < a.length; i++) {
-            // bList max 구하기
             int max = Collections.max(bList);
-
-            // a의 최소값 * b의 최대값 더해주기
-            answer += a[i] * max;
-
-            // bList max 원소 삭제 (==bList.remove(new Integer(max));
+            answer += (a[i] * max);
             bList.remove(Integer.valueOf(max));
         }
 
--- src/programmers/ArcheryCompetition.java
+++ src/programmers/ArcheryCompetition.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 class ArcheryCompetition {
 
-    // 점수, 횟수 나누는 객체
     static class Point {
         int point, cnt;
 
@@ -16,7 +15,6 @@ class ArcheryCompetition {
         }
     }
 
-    // 정답 체크 객체
     static class AnswerCheck {
         int difference;
         int[] ryan;
@@ -29,7 +27,6 @@ class ArcheryCompetition {
 
     static List<AnswerCheck> answer;
 
-    // 중복조합, 경우의수 체크 메소드
     public static void DFS(int cnt, int n, int s, Point[] apeach, Point[] ryan) {
         if (cnt == n) {
             calc(apeach, ryan);
@@ -42,7 +39,6 @@ class ArcheryCompetition {
         }
     }
 
-    // 점수 계산, answer에 추가하는 메소드
     public static void calc(Point[] apeach, Point[] ryan) {
         int aPoint = 0;
         int rPoint = 0;
@@ -52,29 +48,22 @@ class ArcheryCompetition {
             else rPoint += ryan[i].point;
         }
 
-        // 어피치점수가 더 높거나 같다면 return
         if (aPoint >= rPoint) return;
 
-        // 현재 점수차이
         int curDiff = rPoint - aPoint;
 
-        // 현재 ryan 배열, Point[] -> int[]
         int[] curRyan = new int[ryan.length];
         for (int i=0; i<ryan.length; i++) {
             curRyan[i] = ryan[i].cnt;
         }
 
 
-        // answer이 있을때
         if (!answer.isEmpty()) {
 
-            // answerCheck 객체 가져오기
             AnswerCheck answerCheck = answer.get(0);
 
-            // 기존에 차이값이 현재 차이값보다 크다면 return
             if (answerCheck.difference > curDiff) return;
 
-                // 기존에 차이값이 현재 차이값보다 작다면 answer 초기화
             else if (answerCheck.difference < curDiff) answer.clear();
         }
 
@@ -84,28 +73,23 @@ class ArcheryCompetition {
     public int[] solution(int n, int[] info) {
 
         answer = new ArrayList<>();
-        // 객체 선언
+
         Point[] ryan = new Point[11];
         Point[] apeach = new Point[11];
 
-        // 점수, 횟수 저장
         for (int i = 0; i < 11; i++) {
             apeach[i] = new Point(10 - i, info[i]);
             ryan[i] = new Point(10 - i, 0);
         }
 
-        // ryan 모든 경우의수 찾기
         DFS(0, n, 0, apeach, ryan);
 
-        // 답이 없다면 -1 리턴
         if (answer.isEmpty()) return new int[]{-1};
 
-        // 정답 조건
         Collections.sort(answer, (o1, o2) -> {
             int[] aRyan = o1.ryan;
             int[] bRyan = o2.ryan;
             for (int i = 10; i >= 0; i--) {
-                // 두 배열의 횟수가 다른경우 양수 리턴으로 순서 바꿈, 큰 횟수부터 반환
                 if (aRyan[i] != bRyan[i]) return bRyan[i] - aRyan[i];
             }
             return 0;
